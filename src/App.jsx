ab86c3c5c04ecf69947bc927bb566d5839794565
Lucas Rolando <lucasrls98@gmail.com>
2023-10-15 19:07:28 -0300

    feat: ordem alfabética

--- src/App.jsx
+++ src/App.jsx
@@ -69,7 +69,7 @@ function App() {
     <div className="app">
       <h1>Lista de Tarefas</h1>
       <Search search={search} setSearch={setSearch} />
-      <Filter filter={filter} setFilter={setFilter} />
+      <Filter filter={filter} setFilter={setFilter} setSort={setSort} />
       <div className="todo-list">
         {todos
           .filter((todo) =>
@@ -82,6 +82,11 @@ function App() {
           .filter((todo) =>
             todo.text.toLowerCase().includes(search.toLowerCase())
           )
+          .sort((a, b)=>
+            sort === "Asc"
+              ? a.text.localeCompare(b.text)
+              : b.text.localeCompare(a.text)
+            )
           .map((todo) => (
             <Todo key={todo.id} todo={todo} removeTodo={removeTodo}
               completeTodo={completeTodo} />
--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,4 +1,4 @@
-const Filter = ({filter, setFilter}) => {
+const Filter = ({filter, setFilter, setSort}) => {
     return (
         <div className="filter">
             <h2>📌Filtrar: </h2>
@@ -13,8 +13,8 @@ const Filter = ({filter, setFilter}) => {
                 </div>
                 <div>
                     <p>Ordem Alfabética: </p>
-                    <button>Asc</button>
-                    <button>Descendente</button>
+                    <button onClick={()=> setSort("Asc")}>Asc</button>
+                    <button onClick={()=> setSort("Desc")}>Desc</button>
                 </div>
             </div>
         </div>
